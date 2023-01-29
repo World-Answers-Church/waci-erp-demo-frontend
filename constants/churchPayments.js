485a77a3e6b2fbf725cfd7f8daee28081ab0b365
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2023-01-29 10:14:11 +0300

    initial commit
    some validations are pending

---
+++ constants/churchPayments.js
@@ -0,0 +1,12 @@
+export const churchPayments = [
+    { person: 'John Smith', amount: 10000, date: '2022-01-01' },
+    { person: 'Jane Doe', amount: 5000, date: '2022-01-02' },
+    { person: 'Bob Johnson', amount: 7500, date: '2022-01-03' },
+    { person: 'Emily Davis', amount: 12500, date: '2022-01-04' },
+    { person: 'Michael Brown', amount: 8500, date: '2022-01-05' },
+    { person: 'Sarah Miller', amount: 9500, date: '2022-01-06' },
+    { person: 'David Garcia', amount: 11000, date: '2022-01-07' },
+    { person: 'Jessica Wilson', amount: 9000, date: '2022-01-08' },
+    { person: 'Matthew Moore', amount: 8000, date: '2022-01-09' },
+    { person: 'Ashley Taylor', amount: 12000, date: '2022-01-10' }
+];
