0f386ad85ff8749f4225dad4d58f8fe0701f6628
Ssembatya Moses <104248822+Mosesjespar@users.noreply.github.com>
2023-01-31 20:46:56 +0300

    added logout option

--- layout/AppTopbar.js
+++ layout/AppTopbar.js
@@ -1,23 +1,38 @@
-import getConfig from 'next/config';
+import getConfig, { setConfig } from 'next/config';
 import Link from 'next/link';
 import Router, { useRouter } from 'next/router';
 import { classNames } from 'primereact/utils';
-import React, { forwardRef, useContext, useImperativeHandle, useRef } from 'react';
+import React, { forwardRef, useContext, useImperativeHandle, useRef, useState } from 'react';
 import { LayoutContext } from './context/layoutcontext';
-
+import { Button } from 'primereact/button';
+import { Dialog } from 'primereact/dialog';
+import Replace from '../utils/replace'
 const AppTopbar = forwardRef((props, ref) => {
     const { layoutConfig, layoutState, onMenuToggle, showProfileSidebar } = useContext(LayoutContext);
     const menubuttonRef = useRef(null);
     const topbarmenuRef = useRef(null);
     const topbarmenubuttonRef = useRef(null);
     const contextPath = getConfig().publicRuntimeConfig.contextPath;
-
+    const [displayConfirmation, setDisplayConfirmation] = useState(false);
+    const router = useRouter();
     useImperativeHandle(ref, () => ({
         menubutton: menubuttonRef.current,
         topbarmenu: topbarmenuRef.current,
         topbarmenubutton: topbarmenubuttonRef.current
     }));
 
+    const logOut = () => {
+        router.push('/');
+        props.setLogged(false);
+        Replace('/')
+    };
+
+    const confirmationDialogFooter = (
+        <>
+            <Button type="button" label="No" icon="pi pi-times" onClick={() => setDisplayConfirmation(false)} className="p-button-text" />
+            <Button type="button" label="Yes" icon="pi pi-check" onClick={() => logOut()} className="p-button-text" autoFocus />
+        </>
+    );
     return (
         <div className="layout-topbar">
             <Link href="/">
@@ -32,7 +47,7 @@ const AppTopbar = forwardRef((props, ref) => {
             <button ref={menubuttonRef} type="button" className="p-link layout-menu-button layout-topbar-button" onClick={onMenuToggle}>
                 <i className="pi pi-bars" />
             </button>
-{/* 
+            {/* 
             <button ref={topbarmenubuttonRef} type="button" className="p-link layout-topbar-menu-button layout-topbar-button" onClick={showProfileSidebar}>
                 <i className="pi pi-ellipsis-v" />
             </button> */}
@@ -42,18 +57,23 @@ const AppTopbar = forwardRef((props, ref) => {
                     <i className="pi pi-calendar"></i>
                     <span>Calendar</span>
                 </button> */}
-                <button type="button" className="p-link layout-topbar-button">
+                <button type="button" className="p-link layout-topbar-button" onClick={() => setDisplayConfirmation(true)}>
                     <i className="pi pi-fw pi-sign-out"></i>
                     <span>LogOut</span>
-          
                 </button>
-               
-                    <button type="button" className="p-link layout-topbar-button">
-                        <i className="pi pi-cog"></i>
-                        <span>Settings</span>
-                    </button>
-                
+
+                <button type="button" className="p-link layout-topbar-button">
+                    <i className="pi pi-cog"></i>
+                    <span>Settings</span>
+                </button>
             </div>
+
+            <Dialog header="Confirmation" visible={displayConfirmation} onHide={() => setDisplayConfirmation(false)} style={{ width: '350px' }} modal footer={confirmationDialogFooter}>
+                <div className="flex align-items-center justify-content-center">
+                    <i className="pi pi-exclamation-triangle mr-3" style={{ fontSize: '2rem' }} />
+                    <span>Are you sure you want to sign out?</span>
+                </div>
+            </Dialog>
         </div>
     );
 });
